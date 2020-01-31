ed7b52e053a7523a82df697d0468757719a75687
tikired <egdoglia@gmail.com>
2020-01-31 16:40:00 -0300

    tutorial ready

--- js/index.js
+++ js/index.js
@@ -16,7 +16,8 @@ var config = {
     }
 }
 
-var player, platforms, cursors, stars
+var player, platforms, cursors, stars, scoreText, bombs
+var score = 0
 
 var game = new Phaser.Game(config)
 
@@ -69,9 +70,13 @@ function create () {
     stars.children.iterate((child) => {
         child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
     })
+    bombs = this.physics.add.group()
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' })
     this.physics.add.collider(stars, platforms)
     this.physics.add.collider(player, platforms)
+    this.physics.add.collider(bombs, platforms)
     this.physics.add.overlap(player, stars, collectStar, null, this)
+    this.physics.add.collider(player, bombs, hitBomb, null, this)
 }
 
 function update () {
@@ -91,5 +96,25 @@ function update () {
 }
 
 const collectStar = (player, star) => {
-    star.disableBody(true, true);
+    star.disableBody(true, true)
+    score += 10
+    scoreText.setText('Score: ' + score)
+    if (stars.countActive(true) === 0) {
+        stars.children.iterate((child) => {
+            child.enableBody(true, child.x, 0, true, true);
+        })
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400)
+        var bomb = bombs.create(x, 16, 'bomb')
+        bomb.setBounce(1)
+        bomb.setCollideWorldBounds(true)
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20)
+    }
 }
+
+function hitBomb (player, bomb) {
+    this.physics.pause()
+    player.setTint(0xff0000)
+    player.anims.play('turn')
+    gameOver = true
+}
+
